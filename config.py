1cd74a1bdb6bdc2d362cf3949b3fd09a428fc63c
Mohamed Hendawy <hendawy@outlook.com>
2014-03-05 17:45:25 +0200

    Remove Config Vars
    Signed-off-by: Mohamed Hendawy <hendawy@outlook.com>

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 config = {
-	'keyword' : '#G2BLast3Nights',
-	'api_secret' : 'CRbXTzdamWWVGFOdBPqcjk5pzbxbm1RWQQAz4xvY',
-	'api_key' : 'pq2LeiIGRxRhAESmdHg',
-	'access_token_secret' : 'rYtcTC8h2lRm30ZQRMob9iUjceHLO0HXdbud8tMf4IjX6',
-	'access_token' : '118719319-2Yutc569eV9vFgWigmRxyODd0Chy7XvKCp5SAC8p',
-	'send_to' : r'http://localhost:8888/a/message/new',
+	'keyword' : '', #Keyword to Track
+	'api_secret' : '', #Twitter Api Secret
+	'api_key' : '', #Twitter Api Key
+	'access_token_secret' : '', #Twitter Access Token Secret
+	'access_token' : '', #Twitter Access Token
+	'send_to' : r'', #The url to which the request will be sent
 }
